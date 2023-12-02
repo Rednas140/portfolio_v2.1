4573ca0e8e71270ef035cea75dbe0e5d19e595f9
Sander Verhoeven <sanderjverhoeven@hotmail.com>
2023-12-02 21:17:37 +0100

    Refractored file system and further setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Syne, Poppins } from 'next/font/google'
+import './main.css'
+
+const syne = Syne({ subsets:['latin'], variable: '--font-syne', });
+const poppins = Poppins({ weight: '400', subsets:['latin'], variable: '--font-poppins' });
+
+export const metadata: Metadata = {
+  title: 'Portfolio Sander Verhoeven',
+  description: 'Het portfolio van Sander Verhoeven een junior front-end developer',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="nl">
+      <body className={`${syne.variable} ${poppins.variable} all:unset`}>{children}</body>
+    </html>
+  )
+}
