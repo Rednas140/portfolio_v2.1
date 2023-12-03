db642c40095ace62230cdb6fec6a170b76bef1b6
Sander Verhoeven <sanderjverhoeven@hotmail.com>
2023-12-03 23:42:16 +0100

    setup component structure and styling

---
+++ app/components/organisms/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <>
+        <div>
+            <h1>
+                Hey there, I'm Sander Verhoeven
+            </h1>
+            <p>
+                A junior developer with a passion for inclusive design and development. Currently based in Papendrecht, The Netherlands.
+            </p>
+            <button>Take a look at my projects</button>
+        </div>
+        </>
+    )
+}
