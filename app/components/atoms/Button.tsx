4672f3c8981fcdc42760b4e4d64e3bbbfba3d2bb
Sander Verhoeven <sanderjverhoeven@hotmail.com>
2026-01-30 23:50:54 +0100

    motion setup + tailwind setup

--- app/components/atoms/Button.tsx
+++ app/components/atoms/Button.tsx
@@ -1,5 +1,16 @@
-export default function Button({link, content, target}:any){
-    return(
-        <a href={link} target={!!target ? target : "_self"} className="c-button">{!!content ? content : "Button text"}</a>
-    )
+"use client";
+import { motion } from "motion/react";
+
+export default function Button({ link, content, target }: any) {
+  return (
+    <motion.a
+      href={link}
+      target={!!target ? target : "_self"}
+      className="c-button inline-block"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      {!!content ? content : "Button text"}
+    </motion.a>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Image from 'next/image';
-import Navbar from './components/organisms/Navbar';
-import Header from './components/organisms/Header';
-import About from './components/organisms/About';
-import Projects from './components/organisms/Projects';
-import Footer from './components/organisms/Footer';
+import Image from "next/image";
+import Navbar from "./components/organisms/Navbar";
+import Header from "./components/organisms/Header";
+import About from "./components/organisms/About";
+import Projects from "./components/organisms/Projects";
+import Footer from "./components/organisms/Footer";
 
 export default function Home() {
   return (
     <main>
-        <Navbar></Navbar>
-      	<Header></Header>
-        <About></About>
-        <Projects></Projects>
-        <Footer></Footer>
+      <Navbar></Navbar>
+      <Header></Header>
+      <About></About>
+      <Projects></Projects>
+      <Footer></Footer>
     </main>
-    )
+  );
 }
