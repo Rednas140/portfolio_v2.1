cf8dc88c41149e966ce14471697430c0a816c61c
Sander Verhoeven <sanderjverhoeven@hotmail.com>
2026-01-31 01:39:56 +0100

    tailwind toggle

--- app/components/atoms/Hamburger.tsx
+++ app/components/atoms/Hamburger.tsx
@@ -4,11 +4,28 @@ import { motion } from "motion/react";
 export default function Hamburger({ handleClick, isOpen }: any) {
   const hamburgerClasses = "block h-8 w-8 relative cursor-pointer lg:hidden";
 
+  const baseLine =
+    "hamburger-line block absolute right-0 bg-slate-900 h-1 transition-all duration-700 ease-spring-soft";
+
+  const closedByIndex = (i: number) => {
+    if (i === 0) return "rotate-0 w-8 top-1.75";
+    if (i === 1) return "rotate-0 w-6 top-3.5";
+    if (i === 2) return "rotate-0 w-6 top-3.5";
+    return "rotate-0 w-4 top-5.25";
+  };
+
+  const openByIndex = (i: number) => {
+    if (i === 0) return "top-4 w-0 right-1/2";
+    if (i === 1) return "rotate-45 w-8";
+    if (i === 2) return "-rotate-45 w-8";
+    return "top-4 w-0 right-1/2";
+  };
+
   return (
     <motion.button
       type="button"
       onClick={handleClick}
-      className={`hamburger ${hamburgerClasses} ${isOpen ? "open" : ""} [--entry-distance-x:20px]`}
+      className={`${hamburgerClasses} [--entry-distance-x:20px] w-`}
       aria-label="Hoofdmenu toggle"
       id="hamburger"
       initial={{ opacity: 0, x: "var(--entry-distance-x)" }}
@@ -17,8 +34,10 @@ export default function Hamburger({ handleClick, isOpen }: any) {
       {Array.from({ length: 4 }).map((_, index) => (
         <span
           key={index}
-          className="hamburger-line block absolute right-0 rotate-0 bg-slate-900 h-1 transition-transform duration-700 ease-spring-soft"
-        ></span>
+          className={`${baseLine} ${
+            isOpen ? openByIndex(index) : closedByIndex(index)
+          }`}
+        />
       ))}
     </motion.button>
   );
--- app/components/atoms/Navlink.tsx
+++ app/components/atoms/Navlink.tsx
@@ -1,5 +1,13 @@
-export default function Navlink({link, content} : any){
-    return(
-        <a href={link} className="c-navbar-list-item">{!!content ? content : "Navlink text"}</a>
-    )
+"use client";
+import { motion } from "motion/react";
+
+export default function Navlink({ link, content }: any) {
+  return (
+    <motion.a
+      href={link}
+      className="font-syne text-4xl lg:text-xl font-bold text-white lg:text-slate-900 decoration-0 cursor-pointer hover"
+    >
+      {!!content ? content : "Navlink text"}
+    </motion.a>
+  );
 }
