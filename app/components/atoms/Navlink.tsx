c94824af723cfa2fa5a8d8d5acc813814130da0f
Sander <sanderjverhoeven@hotmail.com>
2026-02-01 16:02:48 +0100

    feat: added hover state

--- app/components/atoms/Navlink.tsx
+++ app/components/atoms/Navlink.tsx
@@ -10,7 +10,7 @@ export default function Navlink({ link, content }: NavlinkProps) {
   return (
     <motion.a
       href={link}
-      className="font-syne text-4xl lg:text-xl font-bold text-white lg:text-neutral-900 decoration-0 cursor-pointer hover"
+      className="font-syne text-4xl lg:text-xl font-bold text-white lg:text-neutral-900 decoration-0 cursor-pointer hover:decoration-2 hover:underline underline-offset-8"
     >
       {!!content ? content : "Navlink text"}
     </motion.a>
