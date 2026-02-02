edab29b8cf020cd661050364891bef184bc1e1a3
Sander Verhoeven <sanderjverhoeven@hotmail.com>
2026-02-03 00:39:59 +0100

    feat: added navlink animation

--- app/components/molecules/Navlist.tsx
+++ app/components/molecules/Navlist.tsx
@@ -1,13 +1,46 @@
+import { motion, stagger } from "motion/react";
 import Navlink from "../atoms/Navlink";
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      delayChildren: stagger(0.1),
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
+
+const item = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: -100 },
+};
 
 export default function NavItems({ isOpen }: any) {
   return (
-    <div
+    <motion.ul
+      initial="hidden"
+      whileInView="visible"
+      variants={list}
       className={`hidden md:flex flex-row items-center gap-8 ${isOpen ? "flex bg-neutral-900 md:bg-white absolute md:static flex-col md:flex-row justify-center md:justify-start top-100 left-0 w-screen md:w-auto h-1/2 md:h-auto" : ""}`}
     >
-      <Navlink link="#about" content="About"></Navlink>
-      <Navlink link="#projects" content="Projects"></Navlink>
-      <Navlink link="#footer" content="Contact"></Navlink>
-    </div>
+      <motion.li variants={item}>
+        <Navlink link="#about" content="About"></Navlink>
+      </motion.li>
+
+      <motion.li variants={item}>
+        <Navlink link="#projects" content="Projects"></Navlink>
+      </motion.li>
+
+      <motion.li variants={item}>
+        <Navlink link="#footer" content="Contact"></Navlink>
+      </motion.li>
+    </motion.ul>
   );
 }
--- app/components/organisms/Header.tsx
+++ app/components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import HeaderImages from "../molecules/HeaderImages";
 
 export default function Header() {
   return (
-    <header className="flex pt-4 px-4 pb-8 gap-6 items-center justify-center flex-col md:pt-6 md:px-12 md:pb-18 lg:flex-row lg:justify-between lg:min-h-[calc(100vh - 71px)] lg:px-28 lg:pb-24">
+    <header className="flex pt-4 px-4 pb-8 gap-6 items-center justify-center flex-col md:pt-6 md:px-12 md:pb-18 lg:flex-row lg:justify-between lg:min-h-[calc(100vh - 71px)] lg:px-28 lg:pb-24 xl:px-53">
       <HeaderContent></HeaderContent>
       <HeaderImages></HeaderImages>
     </header>
